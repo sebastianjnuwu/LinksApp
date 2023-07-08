95554c8aa4601bfcd97f1ebce1159a1fb60c2ee8
SEBASTIAN JN <sebastianjnuwu@gmail.com>
2023-07-08 17:31:48 -0300

    update src/Components/ToggleMode.svelte<script lang="ts">   import Fa from "svelte-fa";   import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";   import $ from "jquery";   import Cookies from "js-cookie";    let darkMode = true;    const handleToggle = () => {     darkMode = !darkMode;     toggleDarkMode();   };    const toggleDarkMode = () => {     if (darkMode) {       $("body").addClass("dark");       $("#color-theme").attr("content", "#1A202C");       Cookies.set("theme", "dark");     } else {       $("body").removeClass("dark");       $("#color-theme").removeAttr("content");       Cookies.remove("theme");     }   };    const theme = Cookies.get("theme");   if (theme === "dark") {     darkMode = true;     $("body").addClass("dark");     $("#color-theme").attr("content", "#1A202C");   } else {     darkMode = false;   } </script>  <div id="btn-toggle">   <button on:click={handleToggle} id="toggleBtn">     {#if darkMode}       <Fa icon={faMoon} />     {:else}       <Fa icon={faSun} />     {/if}   </button> </div>  <style>   #btn-toggle button {     border: 0;     border-radius: 5px;     position: absolute;     right: 0;     bottom: 40px;     margin-right: 1rem;     padding-bottom: 1rem;     background: transparent;   } </style>

--- src/Components/ToggleMode.svelte
+++ src/Components/ToggleMode.svelte
@@ -1,45 +1,55 @@
 <script lang="ts">
   import Fa from "svelte-fa";
   import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
-  let darkMode = false;
+  import $ from "jquery";
+  import Cookies from "js-cookie";
+
+  let darkMode = true;
+
   const handleToggle = () => {
     darkMode = !darkMode;
-    window.document.body.classList.toggle("dark");
-    if (window.document.body.classList.value === "dark") {
-      window.document
-        .querySelector("#color-theme")
-        .setAttribute("content", "#1A202C");
-      localStorage.setItem("theme", "dark");
+    toggleDarkMode();
+  };
+
+  const toggleDarkMode = () => {
+    if (darkMode) {
+      $("body").addClass("dark");
+      $("#color-theme").attr("content", "#1A202C");
+      Cookies.set("theme", "dark");
     } else {
-      window.document.querySelector("#color-theme").removeAttribute("content");
-      localStorage.removeItem("theme");
+      $("body").removeClass("dark");
+      $("#color-theme").removeAttr("content");
+      Cookies.remove("theme");
     }
   };
 
-  if (localStorage.getItem("theme") === "dark") {
-    darkMode = false;
-  } else {
+  const theme = Cookies.get("theme");
+  if (theme === "dark") {
     darkMode = true;
+    $("body").addClass("dark");
+    $("#color-theme").attr("content", "#1A202C");
+  } else {
+    darkMode = false;
   }
 </script>
 
 <div id="btn-toggle">
   <button on:click={handleToggle} id="toggleBtn">
     {#if darkMode}
-      <Fa icon={faSun} />
-    {:else}
       <Fa icon={faMoon} />
+    {:else}
+      <Fa icon={faSun} />
     {/if}
   </button>
 </div>
 
 <style>
   #btn-toggle button {
-    border: 0px;
+    border: 0;
     border-radius: 5px;
     position: absolute;
     right: 0;
-    bottom: 40;
+    bottom: 40px;
     margin-right: 1rem;
     padding-bottom: 1rem;
     background: transparent;
